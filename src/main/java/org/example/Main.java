919dce4e59c93ac8fabec59280a8990a3f24872b
Yusi-Tau <sf5386@gmail.com>
2025-08-03 15:33:20 +0900

    他のものを追加

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
+// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+public class Main {
+    public static void main(String[] args) {
+        //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
+        // IntelliJ IDEA によるその修正案を確認できます。
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+
+
+
+
+    }
+}
